2ee9af9be72d04e3731758b0dac67d8fad5ac0af
sohaibkhan123 <95043904+sohaibkhan123@users.noreply.github.com>
2026-01-22 08:18:08 +0300

    Update iframe height in AIChef component

--- components/AIChef.tsx
+++ components/AIChef.tsx
@@ -1,6 +1,9 @@
+import React from 'react';
+import { ChefHat } from 'lucide-react';
 
+const AIChef: React.FC = () => {
   return (
-   <iframe src="https://tasteshift-10012261.chipp.ai" height="800px" width="100%" frameborder="0" title="Taste Shift" allow="microphone; camera"></iframe>
+    <iframe src="https://tasteshift-10012261.chipp.ai" height="100%" width="100%" frameborder="0" title="Taste Shift" allow="microphone; camera"></iframe>
   );
 };
 
